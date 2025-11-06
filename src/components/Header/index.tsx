f5daa5c0e640facf791adcc3b0de2c0d83ec189e
Gustavo Guedes <gugaguedes11@gmail.com>
2025-11-06 20:23:05 -0300

    Ajuste

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export function Header() {
                 sm:text-5xl/normal sm:py-10 
                 md:text-6xl/normal md:py-11 
                 lg:text-7xl/normal lg:py-12">
-                <Link href="#">Car Sale</Link>
+                <Link href="/">Car Sale</Link>
             </h1>
         </header>
     )
