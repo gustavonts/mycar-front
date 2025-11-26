9622aaf015f2f28b8aa068702ffea184970f1c74
Gustavo Guedes <gugaguedes11@gmail.com>
2025-11-26 20:02:23 -0300

    Incluido editor markdown

--- src/components/admin/ManageCarForm/index.tsx
+++ src/components/admin/ManageCarForm/index.tsx
@@ -3,12 +3,20 @@
 import { Button } from "@/components/Button";
 import { InputCheckbox } from "@/components/inputs/InputCheckbox";
 import { InputText } from "@/components/inputs/InputText";
+import { MarkdownEditor } from "@/components/MarkdownEditor";
+import { SetStateAction, useState } from "react";
 
 export function ManageCarForm() {
+
+    const [contentValue, setContentValue] = useState('Este é um exemplo')
+
     return (
         <form action="" className="mb-16">
             <div className="flex flex-col gap-6">
                 <InputText labelText="Nome" placeholder="Digite seu nome" id={'nome'}/>
+
+                <MarkdownEditor labelText="Conteúdo"  disabled={false} textAreaName="content" value={contentValue} setValue={setContentValue}/>
+
                 <InputCheckbox labelText="Input Teste"/>
                 <div className="mt-4">
                     <Button type="submit" size="md" className="w-full">Enviar</Button>
