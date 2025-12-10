958fa2f1098c779ec9781b00df6d59e6ea9d6b41
Gustavo Guedes <gugaguedes11@gmail.com>
2025-12-10 00:39:14 -0300

    gerar hash e validar senha

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,3 +1,4 @@
+import { verifyPassword } from "@/lib/login/manage-login";
 import { asyncDelay } from "@/utils/async-delay";
 
 type LoginActionState = {
@@ -7,6 +8,36 @@ type LoginActionState = {
 
 export async function loginAction(state: LoginActionState, formData: FormData) {
     await asyncDelay(3000)
+
+    if(!(formData instanceof FormData)) {
+        return {
+            username: '',
+            erros: 'Dados Inválidos'
+        }
+    }
+
+    const username = formData.get('username')?.toString().trim() || ''
+    const password = formData.get('password')?.toString().trim() || ''
+
+    if(!username || !password) {
+        return {
+            username,
+            error: 'Digite o usuário e a senha'
+        }
+    }
+
+    const isUsernameValid = username === process.env.LOGIN_USER
+    const isPasswordValid = await verifyPassword(
+        password,
+        process.env.LOGIN_PASS || ''
+    )
+
+    if(!isUsernameValid && !isPasswordValid) {
+        return {
+            username: '',
+            error: 'Usuário ou senha inválidos'
+        }
+    }
 
     return {
         username: '',
